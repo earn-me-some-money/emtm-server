abdac6a63c0007b9f5785d0ef9abcd6577250176
Miguel Chan <vvchan@outlook.com>
2019-05-14 15:58:12 +0800

    Update CI

---
+++ scripts/setup_test_db.sh
@@ -0,0 +1,7 @@
+cargo install diesel_cli --no-default-features --features mysql
+cd emtm-db
+echo "DATABASE_URL=mysql://travis:@127.0.0.1/EMTM_TEST" >.env
+echo "TEST_DATABASE_URL=mysql://travis:@127.0.0.1/EMTM_TEST" >>.env
+diesel setup
+cd ..
+
